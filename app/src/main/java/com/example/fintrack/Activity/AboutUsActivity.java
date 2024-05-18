af329fa2f0e09e9c99b383b6b89654a0877bd78d
Aryan Mehta <73061750+Aryan-R-Mehta@users.noreply.github.com>
2024-05-18 15:32:27 +0530

    Create AboutUsActivity.java

---
+++ app/src/main/java/com/example/fintrack/Activity/AboutUsActivity.java
@@ -0,0 +1,30 @@
+package com.example.fintrack.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.fintrack.R;
+
+public class AboutusActivity extends AppCompatActivity {
+    ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aboutus);
+        back=findViewById(R.id.Back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back=new Intent(AboutusActivity.this,HomeScreenActivity.class);
+                startActivity(back);
+            }
+        });
+    }
+
+
+}
